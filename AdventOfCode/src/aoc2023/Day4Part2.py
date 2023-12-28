3c62d99c013d87d5f5ebbb44e7a4fcbbb304db37
kasimxo <kasimxo@gmail.com>
2023-12-28 12:19:48 +0100

    took me longer than it should have

--- AdventOfCode/src/aoc2023/Day4Part2.py
+++ AdventOfCode/src/aoc2023/Day4Part2.py
@@ -1,21 +1,49 @@
 f = open("/home/kasimxo/AdventOfCode/Inputs/KaS-AOC-Inputs/2023/D4")
 
-def checkLine(winningNumbers, haveNumbers):
-	value = 0
-	for wN in winningNumbers:
-		if wN in haveNumbers:
-			if value == 0:
-				value = 1
-			else:
-				value *= 2
-	return value
+def procesar(ID, values):
+	repeticiones = 0
+	for n in values[0].split():
+		if n in values[1].split():
+			repeticiones += 1
+	print("Winning numbers: ", repeticiones)
+	return repeticiones
 
-totValue = 0
+def sumRep(cards, ID, IDcurr):
+	currVal = list(cards.get(str(ID)))
+	#print(currVal[2])
+	currVal[2] = int(currVal[2]) + cards[IDcurr][2] 
+	cards[ID] = tuple(currVal)
+	return cards[ID]
+	#print(currVal)
 
+#Here we store cards with this struct: ID, winningNumbers, Numbers, Repeats
+cards = {}
+
+totCartas = 0
+maxID = 0
 
 for line in f.readlines():
-	numbers = line.split(':')[1].split('|')
-	totValue += checkLine(numbers[0].split(), numbers[1].split())
+	splitted = line.split(':')
+	IDRAW = splitted[0].split()[1]
+	ID = IDRAW[0:len(IDRAW)]
+	Numbers = splitted[1].split('|')
+	cards[ID] = Numbers[0], Numbers[1][0:len(Numbers[1])-1], 1
+	maxID = ID
 
-print(totValue)
+for x in range(int(maxID)):
+	k = str(x+1)
+	#n = numero de cartas a duplicar
+	n = procesar(k, cards[k])
+	totCartas += cards[k][2]
+	print("Cartas a sumar: ", n,"Nº carta:", x+1,"Copias: ", cards[k][2])
+	for i in range(n):
+		i += 1
+		try:
+			cards[str(i+int(k))] = sumRep(cards, i+int(k), k)
+			#print(i+int(k),cards[str(i+int(k))])
+		except:
+			pass
+	print(cards[k])
 
+#print(cards)
+print(totCartas)

a925b420d46c030921a84a16fcb05ba1ba6b7858
kasimxo <kasimxo@gmail.com>
2024-11-04 09:51:28 +0100

    2016 D4 P2

---
+++ AdventOfCode/js/aoc2016/04/Day4_P2.js
@@ -0,0 +1,93 @@
+const { readLines } = require('./input')
+var texto = readLines()
+
+let num = 0 // Suma de las ids de las habitaciones válidas
+
+//recorrer todas las habitaciones
+//Identificar si son válidas
+//Sumar el id
+
+texto.forEach((linea) => {
+    if (validarHabitacion(linea)) {
+        //num += sumarId(linea)
+        //mostrar el id
+        let id = identificarId(linea)
+        //mostrar el nombre de la hab
+        let nombre = calcularNombre(linea, id)
+
+        console.log(nombre, id)
+    }
+})
+
+console.log(num)
+
+function validarHabitacion(linea) {
+    let divs = linea.replaceAll('-', '').split('[')
+    let parte1 = divs[0]
+    let parte2 = divs[1].replaceAll(']', '')
+    let letras = {}
+    for (let i = 0; i < parte1.length; i++) {
+        if (Number.isNaN(Number.parseInt(parte1[i]))) {
+            if (letras.hasOwnProperty(parte1[i])) {
+
+                letras[parte1[i]] = letras[parte1[i]] + 1
+            } else {
+                letras[parte1[i]] = 1
+            }
+        }
+    }
+
+    //Almacenamos las letras ordenadas por cantidad, en caso de empate, por orden alfabetico
+    let letrasOrdenadas = []
+    Object.keys(letras).forEach((key) => {
+        letrasOrdenadas.push([key, letras[key]])
+    })
+
+    letrasOrdenadas = letrasOrdenadas.sort((a, b) => {
+        if (b[1] !== a[1]) {
+            return b[1] - a[1]
+        } else {
+            return a[0].charCodeAt(0) - b[0].charCodeAt(0)
+        }
+
+    })
+
+    for (let i = 0; i < parte2.length; i++) {
+        if (parte2[i].localeCompare(letrasOrdenadas[i][0]) !== 0) {
+            return false
+        }
+    }
+    return true
+}
+
+function sumarId(linea) {
+    let sum = '0'
+    for (let i = 0; i < linea.length; i++) {
+        if (!Number.isNaN(Number.parseInt(linea[i]))) {
+            sum += linea[i]
+        }
+    }
+    return Number.parseInt(sum)
+}
+
+function identificarId(linea) {
+    let sum = '0'
+    for (let i = 0; i < linea.length; i++) {
+        if (!Number.isNaN(Number.parseInt(linea[i]))) {
+            sum += linea[i]
+        }
+    }
+    return Number.parseInt(sum)
+}
+
+function calcularNombre(linea, id) {
+    linea = linea.replaceAll('-', '')
+    let abc = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+    let cadena = ''
+    for (let i = 0; i < linea.length; i++) {
+        if (abc.indexOf(linea[i]) >= 0) {
+            cadena += abc[abc.indexOf(linea[i]) + id % 26]
+        }
+    }
+    return cadena
+}

70cfdfa4bad383cc2900571f0cafcd9ad445e0c3
kasimxo <kasimxo@gmail.com>
2024-11-06 10:48:23 +0100

    2016 D7

---
+++ AdventOfCode/js/aoc2016/07/Day7_P1.js
@@ -0,0 +1,41 @@
+const { readLines } = require('./input')
+var text = readLines()
+/*
+text = [
+    'abba[mnop]qrst',
+    'abcd[bddb]xyyx',
+    'aaaa[qwer]tyui',
+    'ioxxoj[asdfgh]zxcvbn'
+]
+*/
+let count = 0
+text.forEach((ip) => {
+    //Validar que las letras FUERA de corchetes cumplen con ABBA
+    //Validar que las letras DENTRO de conchetes NO cumplen con ABBA
+    //let valid = validateString(ip)
+    let valid = false
+    let outside = ''
+    let inside = ''
+    let sections = ip.split(/[\[\]]+/)
+    sections.forEach((part, i) => {
+        if (i % 2 === 0) {
+            outside += part + '----'
+        } else {
+            inside += part + '----'
+        }
+    })
+    if (validateString(outside) && !validateString(inside)) {
+        count++
+        valid = true
+    }
+})
+console.log('Solución: ', count)
+
+function validateString(string) {
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[i + 3] && string[i + 1] === string[i + 2] && string[i] !== string[i + 1]) {
+            return true
+        }
+    }
+    return false
+}
---
+++ AdventOfCode/js/aoc2016/07/Day7_P2.js
@@ -0,0 +1,44 @@
+const { readLines } = require('./input')
+var text = readLines()
+/*
+text = [
+    'aba[bab]xyz',
+    'xyx[xyx]xyx',
+    'aaa[kek]eke',
+    'zazbz[bzb]cdb'
+]
+*/
+let count = 0
+text.forEach((ip) => {
+    let valid = false
+    let outside = ''
+    let inside = ''
+    let sections = ip.split(/[\[\]]+/)
+    sections.forEach((part, i) => {
+        if (i % 2 === 0) {
+            outside += part + '----'
+        } else {
+            inside += part + '----'
+        }
+    })
+    let matches = returnMatch(outside)
+    matches.forEach((match) => {
+        if (inside.indexOf(match) >= 0 && valid === false) {
+            valid = true
+            count++
+        }
+    })
+
+    console.log(ip, valid)
+})
+console.log('Solución: ', count)
+
+function returnMatch(string) {
+    let matches = []
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[i + 2] && string[i] !== string[i + 1]) {
+            matches.push(string[i + 1] + string[i] + string[i + 1])
+        }
+    }
+    return matches
+}

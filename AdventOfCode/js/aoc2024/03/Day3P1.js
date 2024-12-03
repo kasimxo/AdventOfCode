87e58615b306b89c86f0dea4fad36c63285e78d2
kasimxo <kasimxo@gmail.com>
2024-12-03 10:02:56 +0100

    2024 D3

---
+++ AdventOfCode/js/aoc2024/03/Day3P1.js
@@ -0,0 +1,40 @@
+const { readLines } = require('./input')
+
+let text = readLines()
+
+text = [
+    'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
+]
+
+
+let solution = 0
+
+let expression = /mul\([0-9]{1,3},[0-9]{1,3}\)/g
+let expressionNums = /[0-9]{1,3}/g
+let expressionDo = /do\(\)/g
+let expressionDont = /don't\(\)/g
+let matches = []
+let matchesDo = []
+let matchesDont = []
+let matchesMixed = []
+
+text.forEach((line) => {
+    let matchesInLine = [...line.matchAll(expression)]
+    matchesInLine.forEach((match) => matches.push(match))
+    let matchesInLineDo = [...line.matchAll(expressionDo)]
+    matchesInLineDo.forEach((match) => matchesDo.push(match))
+    matchesInLineDo.forEach((match) => matchesMixed.push(match))
+    let matchesInLineDont = [...line.matchAll(expressionDont)]
+    matchesInLineDont.forEach((match) => matchesDont.push(match))
+    matchesInLineDo.forEach((match) => matchesMixed.push(match))
+})
+
+console.log(matchesMixed)
+/*
+matches.forEach((match) => {
+    console.log(match.index)
+    let nums = [...match[0].matchAll(expressionNums)]
+    solution += Number.parseInt(nums[0]) * Number.parseInt(nums[1])
+})
+*/
+console.log('Solución: ', solution)
---
+++ AdventOfCode/js/aoc2024/03/Day3P2.js
@@ -0,0 +1,37 @@
+const { readLines, readInput } = require('./input')
+
+let text = readInput()
+/*
+text = [
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+]
+*/
+// 59140341 too high
+// 59140000 too high
+// 56275602
+// 55256196 too low
+let solution = 0
+
+let expression = /mul\([0-9]{1,3},[0-9]{1,3}\)/g
+let expressionNums = /[0-9]{1,3}/g
+
+let parts = text.split("don't()")
+console.log(parts[0])
+let cleanParts = [parts[0]]
+parts.forEach((part) => {
+    let index = part.indexOf('do()')
+    if (index >= 0) {
+        cleanParts.push(part.substring(index))
+    }
+})
+let matches = []
+cleanParts.forEach((part) => {
+    let matchesInLine = [...part.matchAll(expression)]
+    matchesInLine.forEach((match) => matches.push(match))
+})
+
+matches.forEach((match) => {
+    let nums = [...match[0].matchAll(expressionNums)]
+    solution += Number.parseInt(nums[0]) * Number.parseInt(nums[1])
+})
+console.log('Solución: ', solution)

64eadac12a8832726a9fe8384ff1135465b01040
kasimxo <kasimxo@gmail.com>
2024-12-09 11:06:03 +0100

    2024 D9 P1

---
+++ AdventOfCode/js/aoc2024/09/Day9_P1.js
@@ -0,0 +1,42 @@
+const { readInput } = require('./input')
+
+let text = readInput()
+
+let count = 0
+let expandedText = ''
+let fileId = 0
+for (let i = 0; i < text.length; i++) {
+    if (i % 2 === 0) {
+        // FILE
+        for (let j = 0; j < Number.parseInt(text[i]); j++) {
+            expandedText += Math.floor(i / 2) + ";"
+        }
+
+        fileId++
+    } else {
+        // FREE MEMORY
+        for (let j = 0; j < Number.parseInt(text[i]); j++) {
+            expandedText += ".;"
+        }
+    }
+}
+let l = 0
+let arr = expandedText.split(';')
+let r = arr.length - 2
+console.log(arr)
+while (l !== r) {
+    if (arr[l] !== ".") {
+        count += Number.parseInt(arr[l]) * l
+    } else {
+        while (arr[r] === ".") {
+            r--
+        }
+        count += Number.parseInt(arr[r]) * l
+        r--
+    }
+    l++
+}
+if (arr.length % 2 !== 0) {
+    count += Number.parseInt(arr[l]) * l
+}
+console.log('Solucion: ', count, typeof count)

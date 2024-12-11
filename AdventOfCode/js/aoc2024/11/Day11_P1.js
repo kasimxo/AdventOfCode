e2c12a9fcb87fbe9ab57f4548bd1f6aa20f64a0f
kasimxo <kasimxo@gmail.com>
2024-12-11 10:45:21 +0100

    2024 D11

---
+++ AdventOfCode/js/aoc2024/11/Day11_P1.js
@@ -0,0 +1,26 @@
+const { readInput } = require('./input')
+
+let text = readInput()
+
+let stones = []
+
+text.split(" ").forEach((stone) => {
+    stones.push(stone)
+})
+for (let steps = 0; steps < 25; steps++) {
+    let newStones = []
+    for (let i = 0; i < stones.length; i++) {
+        let curr = stones[i]
+        if (curr === "0") {
+            newStones.push("1")
+        } else if (curr.length % 2 === 0) {
+            newStones.push(curr.substring(0, curr.length / 2))
+            let newText = curr.substring(curr.length / 2).replace(/^(0)+/gm, "")
+            newStones.push(newText.length > 0 ? newText : "0")
+        } else {
+            newStones.push(("" + (curr * 2024)))
+        }
+    }
+    stones = newStones
+}
+console.log("Solucion: ", stones.length)
---
+++ AdventOfCode/js/aoc2024/11/Day11_P2.js
@@ -0,0 +1,69 @@
+const { readInput } = require('./input')
+
+let text = readInput()
+
+
+let stones = []
+
+text.split(" ").forEach((stone) => {
+    stones.push(stone)
+})
+
+let count = 0
+
+const MAX_DEPTH = 75
+
+let seen = {}
+
+stones.forEach((rock, index) => {
+    count += calculateLength(rock, 0)
+})
+
+console.log("Solucion: ", count)
+
+function checkSeen(rock, remaining) {
+    if (seen[rock] !== undefined && seen[rock][remaining] !== undefined) {
+        return seen[rock][remaining]
+    } else {
+        return -1
+    }
+}
+
+function setSeen(rock, remaining, length) {
+    if (seen[rock] === undefined) {
+        seen[rock] = {
+            remaining: length
+        }
+    } else {
+        seen[rock][remaining] = length
+    }
+}
+
+function calculateLength(rock, depth) {
+    let preCalculated = checkSeen(rock, depth - MAX_DEPTH + 1)
+    if (preCalculated >= 0) {
+        return preCalculated
+    } else {
+        depth++
+        if (depth <= MAX_DEPTH) {
+            if (rock === "0") {
+                let length = calculateLength("1", depth)
+                setSeen(rock, depth - MAX_DEPTH, length)
+                return length
+            } else if (rock.length % 2 === 0) {
+                let length = calculateLength(rock.substring(0, rock.length / 2), depth)
+                let newText = rock.substring(rock.length / 2).replace(/^(0)+/gm, "")
+                newText = newText.length > 0 ? newText : "0"
+                length += calculateLength(newText, depth)
+                setSeen(rock, depth - MAX_DEPTH, length)
+                return length
+            } else {
+                let length = calculateLength(("" + (rock * 2024)), depth)
+                setSeen(rock, depth - MAX_DEPTH, length)
+                return length
+            }
+        } else {
+            return 1
+        }
+    }
+}

7c29c5a39f0ba4bdca9c49adb24e794efe2ecfc0
kasimxo <kasimxo@gmail.com>
2025-08-22 21:26:21 +0200

    2017 Day 04 P1

---
+++ AdventOfCode/js/aoc2017/04/Day04_P1.js
@@ -0,0 +1,16 @@
+const {readLines} = require('./input')
+
+const lines = readLines()
+let total = 0
+lines.forEach(l => {
+    const words = l.split(' ')
+    const diff = new Set()
+
+    words.forEach(w => {
+        diff.add(w)
+    })
+    if(diff.size === words.length){
+        total++
+    }
+})
+console.log(total)

dd05d779828951b83b9d92c17f139d4da1fad469
kasimxo <kasimxo@gmail.com>
2025-08-23 19:22:28 +0200

    2017 D5 P1

---
+++ AdventOfCode/js/aoc2017/05/Day05_P1.js
@@ -0,0 +1,16 @@
+const {readLines} = require('./input')
+const lines = readLines().map(l => Number.parseInt(l))
+
+let pos = 0
+let steps = 0
+console.log(lines.length)
+while(true){
+    steps++
+    let jump = lines[pos]
+    lines[pos]++
+    pos += jump
+    if(isNaN(lines[pos])) {       
+        console.log(steps)
+        return
+    }
+}

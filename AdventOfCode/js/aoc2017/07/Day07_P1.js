3a9ee38453aa56da4f9d27beca06fc20941efbdd
kasimxo <kasimxo@gmail.com>
2025-08-24 11:40:41 +0200

    2017 D7 P1

---
+++ AdventOfCode/js/aoc2017/07/Day07_P1.js
@@ -0,0 +1,20 @@
+const {readLines} = require('./input')
+
+const lines = readLines().filter(l => l.includes('->'))
+
+const roots = []
+const branches = new Set()
+
+lines.forEach(l => {
+    const words = l.split(' ')
+    roots.push(words[0])
+    for(let i = 3; i<words.length; i++){
+        branches.add(words[i].replace(',', ''))
+    }
+})
+
+roots.forEach(r => {
+    if(!branches.has(r)){
+        console.log(r)
+    }
+})

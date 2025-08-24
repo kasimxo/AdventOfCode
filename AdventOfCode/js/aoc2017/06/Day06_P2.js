05e519d2dfcee0783e2bf6fc0748a0907d64df15
kasimxo <kasimxo@gmail.com>
2025-08-24 10:59:53 +0200

    2017 D6 P2

---
+++ AdventOfCode/js/aoc2017/06/Day06_P2.js
@@ -0,0 +1,36 @@
+const {readInput} = require('./input')
+let input = readInput().split('\t').map(n => Number.parseInt(n))
+const diff = new Set()
+let cycles = {}
+let calculating = true
+let steps = 0
+while (calculating){
+
+    let max = 0
+    let index = 0
+    input.forEach((n, i) => {
+        if(max<n){
+            max = n
+            index = i
+        }
+    })
+
+    input[index] = 0
+    while(max > 0){
+        index++
+        input[index%input.length]++
+        max--
+    }
+    
+    const config = input.join('.')
+
+    if(diff.has(config)){
+        console.log("SOLUTION: ", steps+1)
+        console.log(steps - cycles[config])
+        return
+    } else {
+        diff.add(config)
+        cycles[config] = steps
+    }
+    steps++
+}

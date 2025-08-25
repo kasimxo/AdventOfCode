7f63101182324321ea6514830c0a60a9dc25442e
kasimxo <kasimxo@gmail.com>
2025-08-25 22:25:25 +0200

    2017 D8 P1

---
+++ AdventOfCode/js/aoc2017/08/Day08_P1.js
@@ -0,0 +1,41 @@
+const {readLines} = require('./input')
+
+let registers = {}
+let input = readLines()
+input.forEach(l => {
+    const words = l.split(' ')
+    const register = words[0]
+    if(registers[register] == null){
+        registers[register] = 0
+    }
+    let numA = registers[words[4]] ?? 0
+    let numB = Number.parseInt(words[6])
+    let op = words[5]
+    if(Operation(numA, numB, op)){
+        let sum = Number.parseInt(words[2])
+        registers[register] += words[1] === 'inc' ? sum : sum * -1;
+    }
+})
+
+function Operation(numA, numB, op){
+    switch(op){
+        case '==':
+            return numA == numB;
+        case '>=':
+            return numA >= numB;
+        case '<=':
+            return numA <= numB;
+        case '>':
+            return numA > numB;
+        case '<':
+            return numA < numB;
+        case '!=':
+            return numA != numB;
+        default:
+            console.error("NOT VALID: ", numA, numB, op)
+    }
+}
+
+let max = 0
+Object.entries(registers).forEach(([k, v]) => max = Math.max(v, max))
+console.log(max)

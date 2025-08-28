6122468d34cba8baa0c8b159011f8a8a9e410174
kasimxo <kasimxo@gmail.com>
2025-08-28 21:16:59 +0200

    2016 D12 P1

---
+++ AdventOfCode/js/aoc2016/12/Day12_P1.js
@@ -0,0 +1,50 @@
+const {readLines} = require('./input')
+
+const lines = readLines()
+
+let registers = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0
+}
+
+for(let i = 0; i<lines.length; i++) {
+    const words = lines[i].split(' ')
+    switch(words[0]){
+        case 'inc':
+            inc(words[1])
+            break
+        case 'dec':
+            dec(words[1])
+            break
+        case 'cpy':
+            cpy(words[1], words[2])
+            break
+        case 'jnz':
+            i += jnz(words[1]) ? Number.parseInt(words[2]) - 1 : 0
+            break
+        default:
+            break
+    }
+}
+console.log(registers)
+function inc(register){
+    registers[register]++
+}
+function dec(register){
+    registers[register]--
+}
+function cpy(value, register){
+    if(!Number.isNaN(Number.parseInt(value)))
+        registers[register] = Number.parseInt(value)
+    else 
+        registers[register] = registers[value]
+
+}
+function jnz(value){
+    if(!Number.isNaN(Number.parseInt(value)))
+        return Number.parseInt(value) !== 0
+    
+    return registers[value] !== 0
+}

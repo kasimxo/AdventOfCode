ff656a6286490f9ae305c71b83da50cbe65015f6
kasimxo <kasimxo@gmail.com>
2025-08-30 17:45:16 +0200

    2020 D06 P1

--- AdventOfCode/js/aoc2020/05/Day05_P2.js
+++ AdventOfCode/js/aoc2020/05/Day05_P2.js
@@ -7,16 +7,11 @@ lines.sort((a, b) => b.localeCompare(a))
 let ids = []
 lines.forEach(p => ids.push(calculateID(p.replaceAll('Z', 'L'))))
 function calculateID(pass){
-
     let row = pass.slice(0, 7).replaceAll('B', '1').replaceAll('F', '0')
     let aisle = pass.slice(7, pass.length).replaceAll('R', '1').replaceAll('L', '0')
     let rowNum = Number.parseInt(row, 2)
     let aisleNum = Number.parseInt(aisle, 2)
     let id = rowNum*8+aisleNum
-    if(Number.isNaN(id)){
-        console.log(pass, row, aisle, rowNum, aisleNum, id)
-        throw new Error()
-    }
     return id
 }
 
---
+++ AdventOfCode/js/aoc2020/06/Day06_P1.js
@@ -0,0 +1,14 @@
+const {readInput} = require('./input')
+
+const groups = readInput().split('\r\n\r\n').map(w => w.replaceAll('\r\n', ' '))
+
+let solution = 0
+groups.forEach(g => solution += countAnswers(g))
+console.log(solution)
+
+function countAnswers(group){
+    let letters = new Set()
+    let sheets = group.split(' ')
+    sheets.forEach(s => s.split('').forEach(l => letters.add(l)))
+    return letters.size
+}

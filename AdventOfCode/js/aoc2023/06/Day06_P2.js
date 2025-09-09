5eb49932e81b4964c5f3b4b122904c8b8be9a370
kasimxo <kasimxo@gmail.com>
2025-09-09 21:16:32 +0200

    2023 D06 Very interesting 2nd part with an obvious optimization... that is not necessary

---
+++ AdventOfCode/js/aoc2023/06/Day06_P2.js
@@ -0,0 +1,19 @@
+const {readLines} = require('./input')
+let input = readLines()
+input = input.map(l=>l.split(','))
+let races = []
+for(let i = 1; i<input[0].length; i++){
+    races.push([input[0][i], input[1][i]])
+}
+let options = []
+races.forEach(race=>{
+    let solutions = 0
+    let time = Number.parseInt(race[0])
+    let distance = Number.parseInt(race[1])
+    for(let i = 0; i<time; i++){
+        let remaining = time - i
+        if(i*remaining>distance) solutions++
+    }
+    options.push(solutions)
+})
+console.log(options.reduce((acc, curr)=>acc*=curr))

165451b7916a71824516b6eb9b384e18770c54b0
kasimxo <kasimxo@gmail.com>
2025-09-27 13:15:18 +0200

    2016 D18

---
+++ AdventOfCode/js/aoc2016/18/Day18_P1.js
@@ -0,0 +1,26 @@
+let input = ".^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^...."
+let [SAFE, TRAP] = [true, false]
+let safeCount = input.replaceAll('^', '').length
+for(let row = 0; row<39; row++){
+    let nextRow=''
+    for(let i = 0; i<input.length; i++){
+        let left = i > 0 ? GetPositionStatus(input[i-1]) : SAFE
+        let center = GetPositionStatus(input[i])
+        let right = i<input.length-1 ? GetPositionStatus(input[i+1]) : SAFE
+        if((!left && !center && right)
+        || (left && !center && !right)
+        || (!left && center && right)
+        || (left && center && !right)){
+            nextRow += '^'
+        } else {
+            nextRow += '.'
+            safeCount++
+        }
+    }
+    input = nextRow
+}
+console.log(safeCount)
+
+function GetPositionStatus(cell){
+    return cell === '.' ? SAFE : TRAP
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Progress:<br/>
 
-![](https://img.shields.io/badge/Total%20⭐-242-yellow)
+![](https://img.shields.io/badge/Total%20⭐-244-yellow)
 
 Completed years:
 
